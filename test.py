8b7db0ddb7390bf4bf8e83529fa0724bfa1c1a04
learningman7777 <51466555+learningman7777@users.noreply.github.com>
2020-01-08 23:12:15 +0900

    feat: modify test code to work well

--- test.py
+++ test.py
@@ -6,20 +6,24 @@ import model.loss as module_loss
 import model.metric as module_metric
 import model.model as module_arch
 from parse_config import ConfigParser
-
+from PIL import Image
+from torchvision import transforms
 
 def main(config):
     logger = config.get_logger('test')
 
-    # setup data_loader instances
-    data_loader = getattr(module_data, config['data_loader']['type'])(
-        config['data_loader']['args']['data_dir'],
-        batch_size=512,
-        shuffle=False,
-        validation_split=0.0,
-        training=False,
-        num_workers=2
-    )
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    path = "images/Abyssinian_1.jpg"
+
+    img = Image.open(path)
+    img_transform = transforms.Compose([
+        transforms.Resize((197, 197)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    img_tensor = img_transform(img)
+    img_tensor = img_tensor.unsqueeze(0)
+    img_tensor = img_tensor.to(device)
 
     # build model architecture
     model = config.init_obj('arch', module_arch)
@@ -45,27 +49,18 @@ def main(config):
     total_metrics = torch.zeros(len(metric_fns))
 
     with torch.no_grad():
-        for i, (data, target) in enumerate(tqdm(data_loader)):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
+        data = img_tensor.to(device)
+        output = model(data)
+        output = output.squeeze(-1).squeeze(-1)
+        _, preds = torch.max(output, 1)
+        print(preds)
 
-            #
-            # save sample images, or do something with output here
-            #
-
-            # computing loss, metrics on test set
-            loss = loss_fn(output, target)
-            batch_size = data.shape[0]
-            total_loss += loss.item() * batch_size
-            for i, metric in enumerate(metric_fns):
-                total_metrics[i] += metric(output, target) * batch_size
-
-    n_samples = len(data_loader.sampler)
-    log = {'loss': total_loss / n_samples}
-    log.update({
-        met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
-    })
-    logger.info(log)
+    # n_samples = len(data_loader.sampler)
+    # log = {'loss': total_loss / n_samples}
+    # log.update({
+    #     met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
+    # })
+    # logger.info(log)
 
 
 if __name__ == '__main__':
